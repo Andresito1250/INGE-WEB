c93374167e3fa691f08a336fb1752e9ab9814e75
Andres Felipe Galindo Quiñones <ag702340@gmail.com>
2026-05-24 23:54:24 -0500

    database

--- config/database.php
+++ config/database.php
@@ -1,18 +1,21 @@
 <?php
-$host     = 'localhost';
-$dbname   = 'webapp';
-$user     = 'root';
-$password = 'root';
+
+$host = getenv('DB_HOST');
+$db   = getenv('DB_DATABASE');
+$user = getenv('DB_USERNAME');
+$pass = getenv('DB_PASSWORD');
+$port = getenv('DB_PORT');
 
 try {
     $conn = new PDO(
-        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        "mysql:host=$host;port=$port;dbname=$db",
         $user,
-        $password
+        $pass
     );
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-} catch (PDOException $e) {
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
+?>
