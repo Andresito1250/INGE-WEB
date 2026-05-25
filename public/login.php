c554e52acf3dc2361454ffb34698a59e1f29a70b
Andres Felipe Galindo Quiñones <ag702340@gmail.com>
2026-05-25 00:23:57 -0500

    fix arreg222o33

--- public/login.php
+++ public/login.php
@@ -1,5 +1,12 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
+
+
 if (isset($_SESSION['usuario'])) { header("Location: dashboard.php"); exit; }
 
 $error = "";
